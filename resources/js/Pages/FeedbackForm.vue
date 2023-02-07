055f8f3dc245f5c34640460a9acd6c52e0c110aa
Denis Romanenko <deromanenko@gmail.com>
2023-02-07 09:58:36 +0300

    feat: finish form

--- resources/js/Pages/FeedbackForm.vue
+++ resources/js/Pages/FeedbackForm.vue
@@ -1,28 +1,46 @@
 <template>
     <!-- Main container -->
     <div class="flex h-screen justify-center items-center bg-yellow-50">
-        <!-- Form -->
+
         <div class="w-96 bg-white rounded-xl shadow-xl">
             <div class="text-center text-white font-semibold text-2xl p-4 rounded-t-xl bg-gradient-to-r from-cyan-500 to-blue-500">Оставьте заявку</div>
-            <form action="" class="p-4">
+            
+            <!-- Форма -->
+            <form @submit.prevent="feedback.post('/submit')" class="p-4">
+                <!-- Поле имени-->
                 <label for="name" class="block">
                     <span class="sr-only">Имя</span>
-                    <input type="text" id="name" class="mt-1 block w-full rounded-md border border-slate-300 py-2 pl-4 font-light focus:border-indigo-500 focus:shadow-md placeholder:italic placeholder:text-slate-400" placeholder="Ваше имя">
+                    <input v-model="feedback.name" type="text" id="name" class="mt-1 block w-full rounded-md border border-slate-300 py-2 pl-4 font-light focus:border-indigo-500 focus:shadow-md placeholder:italic placeholder:text-slate-400" placeholder="Ваше имя">
                 </label>
+                <!-- Ошибки поля имени -->
+                <div v-if="feedback.errors.name" class="text-xs text-red-500 mt-1">{{ feedback.errors.name }}</div>
+
+                <!-- Поле электронной почты-->
                 <label for="name" class=" block mt-4">
                     <span class="sr-only">Электронная почта</span>
-                    <input type="text" id="name" class="mt-1 block w-full rounded-md border border-slate-300 py-2 pl-4 font-light focus:border-indigo-500 focus:shadow-md placeholder:italic placeholder:text-slate-400" placeholder="Ваша электронная почта">
+                    <input v-model="feedback.email" type="text" id="name" class="mt-1 block w-full rounded-md border border-slate-300 py-2 pl-4 font-light focus:border-indigo-500 focus:shadow-md placeholder:italic placeholder:text-slate-400" placeholder="Ваша электронная почта">
+                </label>
+                <!-- Ошибки электронной почты -->
+                <div v-if="feedback.errors.email" class="text-xs text-red-500 mt-1">{{ feedback.errors.email }}</div>
+
+                <!-- Поле отзыва -->
+                <label for="name" class=" block mt-4">
+                    <span class="sr-only">Заявка</span>
+                    <textarea v-model="feedback.body" type="text" id="name" class="mt-1 block w-full rounded-md border border-slate-300 py-2 pl-4 font-light focus:border-indigo-500 focus:shadow-md placeholder:italic placeholder:text-slate-400 h-48" placeholder="Ваша заявка (необязательно)"></textarea>
                 </label>
 
-                <label for="name" class=" block mt-4">
-                    <span class="sr-only">Отзыв</span>
-                    <textarea type="text" id="name" class="mt-1 block w-full rounded-md border border-slate-300 py-2 pl-4 font-light focus:border-indigo-500 focus:shadow-md placeholder:italic placeholder:text-slate-400 h-48" placeholder="Отзыв"></textarea>
-                </label>
-                <button type="submit" class="mt-4 w-full rounded-md py-2 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white font-bold hover:opacity-90">Отправить отзыв</button>
+                <button type="submit" class="mt-4 w-full rounded-md py-2 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white font-bold hover:opacity-90">Отправить заявку</button>
             </form>
         </div>
     </div>
 </template>
 
 <script setup>
+    import { useForm } from '@inertiajs/vue3'
+
+    const feedback = useForm({
+        name: null,
+        email: null,
+        body: null
+    })
 </script>
--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,5 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FeedbackController;
 
-Route::get('/', [FeedbackController::class, 'showFeedbackForm']);
+Route::get('/', [FeedbackController::class, 'showFeedbackForm'])->name('feedback.showForm');
+Route::post('/submit', [FeedbackController::class, 'submitFeedback']);
